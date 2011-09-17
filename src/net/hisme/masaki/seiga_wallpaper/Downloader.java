efbce3fb903577ad9e3412afd2b0e02b289aca04
Yamada Masaki <masaki@hisme.net>
2011-09-17 23:32:00 +0900

    remove to Clip

--- src/net/hisme/masaki/seiga_wallpaper/Downloader.java
+++ src/net/hisme/masaki/seiga_wallpaper/Downloader.java
@@ -1,24 +1,17 @@
 package net.hisme.masaki.seiga_wallpaper;
 
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.io.InputStream;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
-
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathFactory;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-
+import java.io.OutputStreamWriter;
+import android.content.Context;
 import android.util.Log;
 
 public class Downloader {
 	private static final String tag = "SeigaWallpaper";
+	private static final String urls_file = "image_urls.txt";
 
 	protected int clip_id;
 
@@ -26,45 +19,39 @@ public class Downloader {
 		this.clip_id = clip_id;
 	}
 
-	public List<String> images() throws Exception {
-		String url = String.format("http://afternoon-snow-8621.heroku.com/%d",
-				this.clip_id);
-		List<String> images = new ArrayList<String>();
-		Document document = DocumentBuilderFactory.newInstance()
-				.newDocumentBuilder().parse(http_get_stream(url));
-		XPath xpath = XPathFactory.newInstance().newXPath();
-		NodeList nodes = (NodeList) xpath.evaluate("/seiga/file", document,
-				XPathConstants.NODESET);
-		int len = nodes.getLength();
+	public void save_image_urls(List<String> image_urls) throws Exception {
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
+				SeigaWallpaper.context().openFileOutput(urls_file,
+						Context.MODE_PRIVATE)));
+		for (String url : image_urls) {
+			writer.write(url);
+			writer.newLine();
+		}
+		writer.close();
+	}
 
-		for (int i = 0; i < len; i++) {
-			images.add(xpath.evaluate("./text()", nodes.item(i)));
+	public ArrayList<String> load_image_urls() throws Exception {
+		ArrayList<String> images = new ArrayList<String>();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				SeigaWallpaper.context().openFileInput(urls_file)));
+		String line = null;
+		while ((line = reader.readLine()) != null) {
+			images.add(line);
 		}
 		return images;
 	}
 
-	public InputStream http_get_stream(String url) throws Exception {
-		return new URL(url).openConnection().getInputStream();
-	}
+	public void download_image(String url) {
 
-	public String http_get(String url) throws Exception {
-		Log.d(tag, "get html: " + url);
-		StringBuffer buf = new StringBuffer();
-		String line = null;
-		BufferedReader reader = new BufferedReader(new InputStreamReader(
-				http_get_stream(url)));
-		while ((line = reader.readLine()) != null) {
-			buf.append(line);
-		}
-		return buf.toString();
 	}
 
 	public static String download(int clip_id) throws Exception {
 		try {
 			Downloader downloader = new Downloader(clip_id);
-			List<String> images = downloader.images();
+			List<String> images = new Clip(clip_id).image_urls();
+			downloader.save_image_urls(images);
 		} catch (Exception e) {
-			Log.d(tag, "Exception: " + e.getMessage());
+			Log.d(tag, String.format("Exception: %s", e.getMessage()));
 		}
 		return "";
 	}
