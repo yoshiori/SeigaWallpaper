0c72ac7ba0e6e844fd454fdda51d2e734f2df6c7
Yamada Masaki <masaki@hisme.net>
2011-09-18 03:02:34 +0900

    add method to get bitmap

--- src/net/hisme/masaki/seiga_wallpaper/SeigaWallpaper.java
+++ src/net/hisme/masaki/seiga_wallpaper/SeigaWallpaper.java
@@ -1,17 +1,34 @@
 package net.hisme.masaki.seiga_wallpaper;
 
 import android.app.Application;
-import android.content.Context;
+import android.graphics.Bitmap;
 
 public class SeigaWallpaper extends Application {
 	protected static SeigaWallpaper self;
+	private ImageUrlList image_url_list;
 
 	public SeigaWallpaper() {
 		super();
 		SeigaWallpaper.self = SeigaWallpaper.this;
 	}
 
-	public static Context context() throws Exception {
+	public static SeigaWallpaper instance() throws Exception {
 		return self;
+	}
+
+	public int clip_id() {
+		return getSharedPreferences("seiga_clip", Application.MODE_PRIVATE)
+				.getInt("id", 0);
+	}
+
+	public ImageUrlList image_url_list() throws Exception {
+		if (image_url_list == null) {
+			this.image_url_list = new ImageUrlList();
+		}
+		return image_url_list;
+	}
+
+	public Bitmap random_image() throws Exception {
+		return new Image(image_url_list().random()).bitmap();
 	}
 }
